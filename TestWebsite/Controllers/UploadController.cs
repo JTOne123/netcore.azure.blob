f3162eab6c46376e60f3f50bd928125b6bb93ad5
Neville <live.neville@live.com>
2018-04-14 01:54:43 -0400

    whole solution

---
+++ TestWebsite/Controllers/UploadController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.Azure.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestWebsite.Models;
+
+namespace TestWebsite.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly BlobManager blobManager;
+
+        public UploadController(BlobManager blobManager)
+        {
+            this.blobManager = blobManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UploadViewModel model)
+        {
+            string fname = await blobManager.AddToContainer("samples", model.File);
+            string fname2 = await blobManager.AddToContainer("samples", model.OtherFile);
+            return View(model);
+        }
+    }
+}
---
+++ TestWebsite/Models/UploadViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebsite.Models
+{
+    public class UploadViewModel
+    {
+
+        public string Title { get; set; }
+
+        public IFormFile File { get; set; }
+
+        public IFormFile OtherFile { get; set; }
+
+    }
+}
